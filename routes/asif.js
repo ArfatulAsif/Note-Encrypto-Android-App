9ada3c9dd9ee08feaa770e54a9efd8c1a359dc4e
Arfatul Islam Asif <awakicde@gmail.com>
2024-06-05 21:47:20 +0600

    updated the subscription

--- routes/asif.js
+++ routes/asif.js
@@ -9,69 +9,7 @@ const axios = require("axios");
    "mobile": "8801688433248"
 }
 */
-router.post("/subscription/otp/request", async (req, res) => {
-    const { appId, password, mobile } = req.body;
-    try {
-        const requestData = {
-            applicationId: `${appId}`,
-            password: `${password}`,
-            subscriberId: `tel:${mobile}`,
-            applicationHash: "abcde",
-            applicationMetaData: {
-                client: "MOBILEAPP",
-                device: "Samsung S10",
-                os: "android 8",
-                appCode:
-                    "https://play.google.com/store/apps/details?id=lk.dialog.megarunlor",
-            },
-        };
 
-        const response = await axios.post(
-            "https://developer.bdapps.com/subscription/otp/request",
-            requestData
-        );
-
-        res.status(response.status).json({
-            statusCode: response.data.statusCode,
-            statusDetail: response.data.statusDetail,
-            referenceNo: response.data.referenceNo,
-            version: response.data.version,
-        });
-    } catch (error) {
-        res.status(500).json({
-            error: "An error occurred while making the request",
-        });
-    }
-});
-// test 
-router.post("/subscription/otp/verify", async (req, res) => {
-    const { appId, password, referenceNo,otp } = req.body;
-    try {
-        const requestData = {
-            applicationId: `${appId}`,
-            password: `${password}`,
-            referenceNo: referenceNo,
-            otp:otp,
-        };
-
-        const response = await axios.post(
-            "https://developer.bdapps.com/subscription/otp/verify",
-            requestData
-        );
-
-        res.status(response.status).json({
-            statusCode: response.data.statusCode,
-            version: response.data.version,
-            subscriptionStatus:response.data.subscriptionStatus,
-            statusDetail:response.data.statusDetail,
-            subscriberId:response.status.subscriberId
-        });
-    } catch (error) {
-        res.status(500).json({
-            error: "An error occurred while making the request",
-        });
-    }
-});
 router.post("/subscription/subscribe", async (req, res) => {
     const { appId, password, mobile } = req.body;
     const action=1;
@@ -100,6 +38,9 @@ router.post("/subscription/subscribe", async (req, res) => {
         });
     }
 });
+
+
+
 router.post("/subscription/unsubscribe", async (req, res) => {
     const { appId, password, mobile} = req.body;
     const action=0;
@@ -128,6 +69,8 @@ router.post("/subscription/unsubscribe", async (req, res) => {
         });
     }
 });
+
+
 router.post("/subscription/status", async (req, res) => {
     const { appId, password, mobile } = req.body;
     try {
